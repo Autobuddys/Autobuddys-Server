b3b1e2fb838ce734d726227ccf2c7d94e7b62570
mansidw <mansi.dwivedi@spit.ac.in>
2022-06-29 10:14:45 +0530

    added s3

--- api/settings.py
+++ api/settings.py
@@ -234,6 +234,8 @@ django_heroku.settings(locals())
 # ROOT_HOSTCONF = 'api.hosts'
 # DEFAULT_HOST = 'www'
 
-
-
-
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID=env('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY=env('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME=env('AWS_STORAGE_BUCKET_NAME')
+AWS_QUERYSTRING_AUTH= False
